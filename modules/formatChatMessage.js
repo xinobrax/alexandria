187de67be90178cc1342cfce83e192bd31ba653d
xinobrax <raphael.wickihalder@gmail.com>
2015-01-12 16:35:11 +0100

    ChatWindow Bug Fixes

--- modules/formatChatMessage.js
+++ modules/formatChatMessage.js
@@ -1,22 +1,26 @@
 module.exports.formatChatMessage = function(message){
+    
+    /*
     if(message.search('youtube.com/watch\\?v=') !== -1){
         message = message.substring(msg.message.search('youtube.com/watch\\?v=')+20)
         message = '<a href=\'https://www.youtube.com/watch?v=' + message + '\' target=\'_blank\'><img src=\'http://img.youtube.com/vi/' + message + '/mqdefault.jpg\' width=\'150\' /></a>'
     }
+    */
     
     message = message.replace(':)', '<img src=\'images/smilies/icon_e_smile.gif\' />')
     message = message.replace(':D', '<img src=\'images/smilies/icon_e_biggrin.gif\' />')
-    message = message.replace(':lol:', '<img src=\'images/smilies/icon_lol.gif\' />')
-    message = message.replace(':|', '<img src=\'images/smilies/icon_neutral.gif\' />')
-    message = message.replace(':facepalm:', '<img src=\'images/smilies/facepalm.gif\' />')
     message = message.replace('O_O', '<img src=\'images/smilies/icon_eek.gif\' />')
     message = message.replace('8)', '<img src=\'images/smilies/icon_cool.gif\' />')
-    message = message.replace(':encryption:', '<img src=\'images/smilies/icon_cool.gif\' />')
-    message = message.replace(':zzz:', '<img src=\'images/smilies/zzz.gif\' />')
-    message = message.replace(':groel:', '<img src=\'images/smilies/groel.gif\' />')
-    message = message.replace(':1thumb:', '<img src=\'images/smilies/1thumb.gif\' />')
-    message = message.replace(':1thumb:', '<img src=\'images/smilies/1thumb.gif\' />')
+    message = message.replace(':|', '<img src=\'images/smilies/icon_neutral.gif\' />')
     message = message.replace('XD', '<img src=\'images/smilies/xd.gif\' />')
+    
+    message = message.replace('FACEPALM', '<img src=\'images/smilies/facepalm.gif\' />')
+    message = message.replace('ENCRYPTION', '<img src=\'images/smilies/icon_cool.gif\' />')
+    message = message.replace('LOL', '<img src=\'images/smilies/icon_lol.gif\' />')
+    message = message.replace('ZZZ', '<img src=\'images/smilies/zzz.gif\' />')
+    message = message.replace('GROEL', '<img src=\'images/smilies/groel.gif\' />')
+    message = message.replace('1THUMB', '<img src=\'images/smilies/1thumb.gif\' />')
+    
     message = message.replace('LQL', '<img src=\'images/smilies/lql.png\' />')
     message = message.replace('ISEE', '<img src=\'images/smilies/isee.png\' />')
     message = message.replace('DAFUQ', '<img src=\'images/smilies/dafuq.png\' />')
--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -51,6 +51,8 @@ $( document ).ready(function() {
         var date = ("0" + new Date().getHours()).slice(-2) + ':' + ("0" + new Date().getMinutes()).slice(-2) + ':' + ("0" + new Date().getSeconds()).slice(-2)
         var message = { timestamp:date, user:$('#user').val(), room:$('#room').val(), message:$('.chatWindowInputField').val(), color:'00ff00' }
         
+        $(this).parent().prev().children().focus()
+        
         chatSpace.emit('chatMessage', message)
         
         $(this).parent('.chatWindowSendButtonDiv').parent('.chatWindowInput').children('.chatWindowInputFieldDiv').children('.chatWindowInputField').val('')
@@ -72,25 +74,47 @@ chatSpace.on('chatMessage', function(msg){
     var message = ''
     message += '<div class=\'chatWindowPost\'>'
     if(msg.user == $('#user').val()){
-        message += '<div class=\'chatWindowPostUser\' style=\'float:right;\' >'
+        
+        message += '<div class=\'chatWindowPostMessageBox\' style=\'text-align:right;\'>'
+        message += '<div class=\'chatWindowPostMessage\'>'
+        message += '<font style=\'color:#' + msg.color + ';font-weight:bold;\'>'
+        message += '[' + msg.timestamp + '] ' + msg.user
+        message += '</font><br/>'
+        message += msg.message
+        message += '</div>'
+        message += '</div>'
+        
+        message += '<div class=\'chatWindowPostUserBox\'>'
+        message += '<div class=\'chatWindowPostUser\'>'
+        message += '<img src=\'http://0.gravatar.com/avatar/c555b3f0b5564bde0eb15bf95f9c6b81?s=64&d=blank&r=X\' />'
+        message += '</div>'
+        message += '</div>'
     }else{
-        message += '<div class=\'chatWindowPostUser\' style=\'float:left;\'>'
+        message += '<div class=\'chatWindowPostUserBox\'>'
+        message += '<div class=\'chatWindowPostUser\'>'
+        message += '<img src=\'http://0.gravatar.com/avatar/c555b3f0b5564bde0eb15bf95f9c6b81?s=64&d=blank&r=X\' />'
+        message += '</div>'
+        message += '</div>'
+        
+        message += '<div class=\'chatWindowPostMessageBox\'>'
+        message += '<div class=\'chatWindowPostMessage\'>'
+        message += '<font style=\'color:#' + msg.color + ';font-weight:bold;\'>'
+        message += '[' + msg.timestamp + '] ' + msg.user
+        message += '</font><br/>'
+        message += msg.message
+        message += '</div>'
+        message += '</div>'
     }
     
-    message += '<img src=\'http://0.gravatar.com/avatar/c555b3f0b5564bde0eb15bf95f9c6b81?s=64&d=blank&r=X\' width=\'40\' />'
-    message += '</div>'
+    
     if(msg.user == $('#user').val()){
-        message += '<div class=\'chatWindowPostMessage\' style=\'float:right;text-align:right;\'>'
+        
 
     }else{
-        message += '<div class=\'chatWindowPostMessage\' style=\'float:left;\'>'
+        
     }
     
-    message += '<font style=\'color:#' + msg.color + ';font-weight:bold;\'>'
-    message += '[' + msg.timestamp + '] ' + msg.user
-    message += '</font><br/>'
-    message += msg.message
-    message += '</div>'
+    
     message += '</div>'
 
     if(msg.room == $('#room').val()){
@@ -109,7 +133,6 @@ chatSpace.on('chatMessage', function(msg){
         $('#' + msg.room + '.navigationMessageCounter').html(messageCounter)
         
         // TODO Chat Box Highlight
-        
     }    
 })
 
@@ -120,6 +143,57 @@ chatSpace.on('chatMessage', function(msg){
 //
 ////////////////////////////////////////////////////////////////////////////////
 
+chatSpace.on('getRoomHistory', function(roomHistory){
+    
+    for(var i in roomHistory){            
+
+        var message = ''
+        message += '<div class=\'chatWindowPost\' style=\'display:block;\'>'
+        if(roomHistory[i]['user'] == $('#user').val()){
+            
+            message += '<div class=\'chatWindowPostMessageBox\' style=\'text-align:right;\'>'
+            message += '<div class=\'chatWindowPostMessage\'>'
+            message += '<font style=\'color:#' + roomHistory[i]['color'] + ';font-weight:bold;\'>'
+            var date = new Date(roomHistory[i]['date'])
+            date = ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ':' + ('0' + date.getSeconds()).slice(-2)
+            message += '[' + date + '] ' + roomHistory[i]['user']
+            message += '</font><br/>'
+            message += '<p style=\'margin:0px;margin-top:4px;\'>' + roomHistory[i]['message'] + '</p>'
+            message += '</div>'
+            message += '</div>'
+            
+            message += '<div class=\'chatWindowPostUserBox\'>'
+            message += '<div class=\'chatWindowPostUser\'>'
+            message += '<img src=\'http://0.gravatar.com/avatar/c555b3f0b5564bde0eb15bf95f9c6b81?s=64&d=blank&r=X\' />'
+            message += '</div>'
+            message += '</div>'
+        }else{
+            message += '<div class=\'chatWindowPostUserBox\'>'
+            message += '<div class=\'chatWindowPostUser\'>'
+            message += '<img src=\'http://0.gravatar.com/avatar/c555b3f0b5564bde0eb15bf95f9c6b81?s=64&d=blank&r=X\' />'
+            message += '</div>'
+            message += '</div>'
+            
+            message += '<div class=\'chatWindowPostMessageBox\'>'
+            message += '<div class=\'chatWindowPostMessage\'>'
+            message += '<font style=\'color:#' + roomHistory[i]['color'] + ';font-weight:bold;\'>'
+            var date = new Date(roomHistory[i]['date'])
+            date = ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ':' + ('0' + date.getSeconds()).slice(-2)
+            message += '[' + date + '] ' + roomHistory[i]['user']
+            message += '</font><br/>'
+            message += '<p style=\'margin:0px;margin-top:4px;\'>' + roomHistory[i]['message'] + '</p>'
+            message += '</div>'
+            message += '</div>'
+            
+        }
+        message += '</div>'
+        
+        $('.chatWindowTable').append(message)        
+    }   
+    $(".chatWindowMessages").scrollTop($(".chatWindowMessages")[0].scrollHeight)
+})
+
+/*
 chatSpace.on('getRoomHistory', function(roomHistory){
     
     for(var i in roomHistory){            
@@ -148,7 +222,8 @@ chatSpace.on('getRoomHistory', function(roomHistory){
         message += '</div>'
         message += '</div>'
 
-        $('.chatWindowMessages').append(message)        
+        $('.chatWindowTable').append(message)        
     }   
     $(".chatWindowMessages").scrollTop($(".chatWindowMessages")[0].scrollHeight)
 })
+*/
